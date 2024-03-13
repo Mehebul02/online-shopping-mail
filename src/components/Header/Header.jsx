fc42b38f74614284b87d59849903671e479b441a
Mehebul02 <mehebul2123@gmail.com>
2024-03-13 15:37:36 +0600

    banner section added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+
+import bg from '../../assets/images/bg.jpg'
+const Header = () => {
+    return (
+        <div >
+           <header className='flex justify-between max-w-6xl mx-auto mt-10 '>
+           <div >
+                <h1 className='text-3xl font-bold text-cyan-800'>Online Shopping Mail</h1>
+            </div>
+            <nav>
+                <ul className='flex justify-between items-center space-x-7'>
+                   <a href=""><button className='text-xl text-white px-6 py-2 rounded-xl bg-cyan-800'>All</button></a>
+                   <a href=""><li className='text-cyan-800'>Recent</li></a>
+                    <a href=""><li className='text-cyan-800'>Shared With me</li></a>
+        
+                    
+                </ul>
+            </nav>
+           
+           </header>
+          <div className='mt-10'>
+          <img className='max-w-6xl mx-auto rounded-lg' src={bg} alt="" />
+          </div>
+        </div>
+        
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
